c3bbb1af35975a03a16f84a281da50d70880b586
morispolanco <morispolanco@gmail.com>
2023-11-04 19:53:37 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -5,10 +5,13 @@ import requests
 api_url = "https://api.respell.ai/v1/run"
 api_key = "260cee54-6d54-48ba-92e8-bf641b5f4805"
 
-st.title("API Respell.ai Demo")
+st.title("Preguntas legales")
 
-# Ingresa la pregunta del usuario en español
+# Ingresa la pregunta del usuario
 user_question = st.text_input("Ingresa tu pregunta en español")
+
+# Agregar un campo para especificar el país
+selected_country = st.text_input("Especifica el país en español")
 
 if st.button("Obtener Respuesta"):
     # Configura los parámetros de la solicitud
@@ -21,7 +24,7 @@ if st.button("Obtener Respuesta"):
     payload = {
         "spellId": "RP0oSnJvS2ONDeTPCOBPZ",
         "spellVersionId": "6m89Lc40p6kW-yS82NK5H",
-        "inputs": {"pregunta": user_question},
+        "inputs": {"pregunta": user_question, "pais": selected_country},
     }
 
     # Realiza la solicitud a la API
@@ -30,8 +33,20 @@ if st.button("Obtener Respuesta"):
     if response.status_code == 200:
         # Obtiene la respuesta en formato JSON
         api_response = response.json()
-        st.subheader("Respuesta:")
-        st.write(api_response)
+
+        if "outputs" in api_response:
+            # Extrae el contenido Markdown de la respuesta JSON
+            markdown_response = api_response["outputs"]
+
+            # Convierte el contenido Markdown en HTML
+            html_response = markdown.markdown(markdown_response)
+
+            st.subheader("Respuesta en Markdown:")
+            # Muestra la respuesta convertida a HTML
+            st.write(html_response, unsafe_allow_html=True)
+
+        else:
+            st.error("La respuesta de la API no contiene contenido Markdown.")
 
     else:
         st.error("Error al obtener datos de la API. Verifica tu configuración y vuelve a intentarlo.")
