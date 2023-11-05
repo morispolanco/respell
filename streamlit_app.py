a144dd4bbb274a37c9f60216607358b8783576fe
morispolanco <morispolanco@gmail.com>
2023-11-05 16:02:31 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,40 @@
+import streamlit as st
 import requests
 import json
-import streamlit as st
 
-st.title("Aplicación con Respell.ai")
+# Título de la aplicación
+st.title("Preguntas legales")
 
-# Campos de entrada
+# Campos de entrada para el usuario
 pregunta = st.text_input("Pregunta", "Escribe tu pregunta aquí")
 pais = st.text_input("País", "Escribe tu país aquí")
 idioma = st.text_input("Idioma", "Escribe tu idioma aquí")
 
-# Botón para enviar la solicitud a la API
-if st.button("Enviar solicitud"):
+# Botón para corregir el texto
+if st.button("Corregir Texto"):
+    # Realizar la solicitud a la API de Respell.ai
     response = requests.post(
         "https://api.respell.ai/v1/run",
         headers={
-            # Esta es tu clave API
             "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
             "Accept": "application/json",
             "Content-Type": "application/json"
         },
         data=json.dumps({
             "spellId": "RP0oSnJvS2ONDeTPCOBPZ",
+            "spellVersionId": "pzXV2-bDEyJAo52mtgKmS",
             "inputs": {
                 "pregunta": pregunta,
-                "pais": pais,
                 "idioma": idioma,
-                
+                "pais": pais
             }
-        }),
+        })
     )
 
-    # Procesar la respuesta aquí
+    # Procesar la respuesta de la API
+    if response.status_code == 200:
+        corrected_text = response.json().get("outputs", {}).get("pregunta", "Error al corregir el texto")
+        st.write("Texto corregido:", corrected_text)
+    else:
+        st.write("Error al enviar la solicitud a la API")
 
