9f35cd823c2692d2d1e67490b929e9a6ea9d0179
morispolanco <morispolanco@gmail.com>
2023-11-05 15:47:09 -0600

    Update streamlit_app.py

--- streamlit_app.py
+++ streamlit_app.py
@@ -1,34 +1,34 @@
-import streamlit as st
 import requests
 import json
+import streamlit as st
 
-# Define la función para llamar a la API de Respell.ai
-def call_respell_api():
-    api_url = "https://api.respell.ai/v1/run"
-    headers = {
-        "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
-        "Accept": "application/json",
-        "Content-Type": "application/json"
-    }
-    input_data = {
-        "spellId": "RP0oSnJvS2ONDeTPCOBPZ",
-        "spellVersionId": "pzXV2-bDEyJAo52mtgKmS",
-        "inputs": {
-            "pregunta": st.text_input("Pregunta", "Example text"),
-            "pais": st.text_input("Pais", "Example text"),
-            "idioma": st.text_input("Idioma", "Example text"),
-            
-            
-        }
-    }
-    
-    response = requests.post(api_url, headers=headers, data=json.dumps(input_data))
-    return response.json()
+st.title("Aplicación con Respell.ai")
 
-# Crear la aplicación Streamlit
-st.title("Aplicación Respell.ai")
+# Campos de entrada
+pregunta = st.text_input("Pregunta", "Escribe tu pregunta aquí")
+pais = st.text_input("País", "Escribe tu país aquí")
+idioma = st.text_input("Idioma", "Escribe tu idioma aquí")
 
-# Llamada a la API y mostrar resultados
-if st.button("Ejecutar Respell.ai"):
-    result = call_respell_api()
-    st.json(result)
+# Botón para enviar la solicitud a la API
+if st.button("Enviar solicitud"):
+    response = requests.post(
+        "https://api.respell.ai/v1/run",
+        headers={
+            # Esta es tu clave API
+            "Authorization": "Bearer 260cee54-6d54-48ba-92e8-bf641b5f4805",
+            "Accept": "application/json",
+            "Content-Type": "application/json"
+        },
+        data=json.dumps({
+            "spellId": "RP0oSnJvS2ONDeTPCOBPZ",
+            "spellVersionId": "pzXV2-bDEyJAo52mtgKmS",
+            "inputs": {
+                "pregunta": pregunta,
+                "idioma": idioma,
+                "pais": pais
+            }
+        }),
+    )
+
+    # Procesar la respuesta aquí
+
